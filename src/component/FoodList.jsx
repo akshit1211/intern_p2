273ec72c655c1dd4d7e25fd4c2c6c34b74d6b7f5
akshit <trehana12@gmail.com>
2024-01-25 13:44:40 +0530

    first commit

---
+++ src/component/FoodList.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+export default function FoodList({item}) {
+    // let item = props.item;
+  return (
+    <>
+    {console.log(item)}
+    <div className='container pt-4'>
+        <h3 className='pb-3'>Food Items</h3>
+        <ul className='list-group'>
+            <li className='list-group-item'>{item[0]}</li>
+            <li className='list-group-item'>{item[1]}</li>
+            <li className='list-group-item'>{item[2]}</li>
+            <li className='list-group-item'>{item[3]}</li>
+            <li className='list-group-item'>{item[4]}</li>
+        </ul>
+      
+    </div>
+    </>
+  )
+}
