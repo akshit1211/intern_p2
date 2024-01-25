55ca365b0f0fd3a644295790d75376f947699625
akshit <trehana12@gmail.com>
2024-01-25 16:45:31 +0530

    --

--- src/component/FoodList.jsx
+++ src/component/FoodList.jsx
@@ -10,11 +10,15 @@ export default function FoodList({item}) {
     <div className='container pt-4'>
         <h3 className='pb-3'>Food Items</h3>
         <ul className='list-group'>
-            <li className='list-group-item'>{item[0]}</li>
+            {/* <li className='list-group-item'>{item[0]}</li>
             <li className='list-group-item'>{item[1]}</li>
             <li className='list-group-item'>{item[2]}</li>
             <li className='list-group-item'>{item[3]}</li>
-            <li className='list-group-item'>{item[4]}</li>
+            <li className='list-group-item'>{item[4]}</li> */}
+            {item.map((x)=> {
+                // eslint-disable-next-line react/jsx-key
+                return <li key={x} className='list-group-item'>{x}</li>;
+            })}
         </ul>
       
     </div>
